d67c49a4223921bbbd66954f2beeb982cc3262a1
Alastair Gilles <agilles91@gmail.com>
2019-12-10 18:47:37 +0100

    beginnings of api interactions

--- src/thermostat_ui.js
+++ src/thermostat_ui.js
@@ -1,37 +1,46 @@
 var thermostat = new Thermostat();
 
 function setDisplay() {
-    $( "#temperature-display" ).text(function() {
-        return thermostat.temperature;
-    });
+  $("#temperature-display").text(thermostat.temperature);
+  $('#temperature-display').attr('class', thermostat.energyUsage());
 
-    $( "#psm-display" ).text(function() {
-        if(thermostat.powerSavingMode) return 'PSM On';
-        return 'PSM Off';
-    });
+  $("#psm-display").text(function() {
+    if(thermostat.powerSavingMode) return 'PSM On';
+    return 'PSM Off';
+  });
 }
 
-$( document ).ready(function() {
+$(document).ready(function() {
+  setDisplay();
+
+  $("#up-btn").click(function(event) {
+    thermostat.up(1);
     setDisplay();
+  });
 
-    $( "#up-btn" ).click(function( event ) {
-        thermostat.up(1);
-        setDisplay();
-    });
+  $("#down-btn").click(function(event) {
+    thermostat.down(1);
+    setDisplay();
+  });
 
-    $( "#down-btn" ).click(function( event ) {
-        thermostat.down(1);
-        setDisplay();
-    });
+  $("#psm-btn").click(function(event) {
+    thermostat.togglePowerMode();
+    setDisplay();
+  });
 
-    $( "#psm-btn" ).click(function( event ) {
-        thermostat.togglePowerMode();
-        setDisplay();
-    });
+  $("#reset-btn").click(function(event) {
+    thermostat.reset();
+    setDisplay();
+  });
 
-    $( "#reset-btn" ).click(function( event ) {
-        thermostat.reset();
-        setDisplay();
-    });
+  $.getJSON("https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=7dca333c2a0290c11d8c820868e8f829", function(data) {
+    var items = [];
+    items.push("<li>" + data["list"][0]["weather"][0]["description"] + "</li>");
+    items.push("<li>" + JSON.stringify(data) + "</li>");
+
+    $("<ul/>", {
+      html: items.join("")
+    }).appendTo("body");
+  });
 
 });
